fb57f31feba1672511fe057bfa43a714010437f0
Joshua Briley <josh@thebrileys.com>
2015-05-02 09:34:01 -0400

    Built kenwood from commit 26cec94 on branch master

--- app/scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://kenwooddennard.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password'])
-
-  .constant('loginRedirectPath', '/login');
